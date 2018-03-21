fbf004abd7b742d192493262451fede1d4ff2e33
Petur Subev <petur.subev@gmail.com>
2018-03-22 00:38:07 +0200

    Add snippets from examples

---
+++ snippets.js
@@ -0,0 +1,58 @@
+window.count = function(key) {
+  var counters = window.counters;
+  if (!counters) { 
+    counters = {};
+  }
+  counters[key] = counters[key] ? counters[key] + 1 : 1;
+  window.counters = counters;
+};
+
+window.expose = function(key, value) {
+  if (!window.exposed) {
+    window.exposed = {};
+  }
+  window.exposed[key] = value;
+};
+
+window.flatten = function(data) {
+    var result = {};
+    function recurse (cur, prop) {
+        if (Object(cur) !== cur) {
+            result[prop] = cur;
+        } else if (Array.isArray(cur)) {
+             for(var i=0, l=cur.length; i<l; i++)
+                 recurse(cur[i], prop + "[" + i + "]");
+            if (l == 0)
+                result[prop] = [];
+        } else {
+            var isEmpty = true;
+            for (var p in cur) {
+                isEmpty = false;
+                recurse(cur[p], prop ? prop+"."+p : p);
+            }
+            if (isEmpty && prop)
+                result[prop] = {};
+        }
+    }
+    recurse(data, "");
+    return result;
+}
+
+window.unflatten = function(data) {
+    "use strict";
+    if (Object(data) !== data || Array.isArray(data))
+        return data;
+    var regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
+        resultholder = {};
+    for (var p in data) {
+        var cur = resultholder,
+            prop = "",
+            m;
+        while (m = regex.exec(p)) {
+            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+            prop = m[2] || m[1];
+        }
+        cur[prop] = data[p];
+    }
+    return resultholder[""] || resultholder;
+};
